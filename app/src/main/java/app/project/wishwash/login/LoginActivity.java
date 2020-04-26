e0e6bdb0663c2be9820c17459b524a596e253542
MikB182 <mikb182@gmail.com>
2020-04-26 20:50:50 +0200

    first login version

--- app/src/main/java/app/project/wishwash/login/LoginActivity.java
+++ app/src/main/java/app/project/wishwash/login/LoginActivity.java
@@ -1,16 +1,78 @@
 package app.project.wishwash.login;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+import app.project.wishwash.MenuActivity;
 import app.project.wishwash.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button signInBtn;
+    //private Button listBtn;
+
+    private static final int RC_SIGN_IN = 1708;
+    private static final String TAG = LoginActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signInBtn = findViewById(R.id.signInBtn);
+        //listBtn = findViewById(R.id.listBtn);
+
+            signInBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                List<AuthUI.IdpConfig> providers = Arrays.asList(
+                        new AuthUI.IdpConfig.EmailBuilder().build());
+
+                startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), RC_SIGN_IN);
+            }
+        });
+
+//            listBtn.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, ListActivity.class));
+//            }
+//        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Log.d(TAG, user.getUid());
+                ((TextView) findViewById(R.id.userId)).setText(user.getUid());
+
+                Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+                startActivity(intent);
+
+            } else {
+                Log.d(TAG, response.getError().getMessage());
+            }
+        }
     }
 }
