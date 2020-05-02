9453b6d44f6511557a2288b089c714290a2d3296
AbdulBarakeh <abdulbarakeh98@gmail.com>
2020-05-02 14:36:01 +0200

    add userlist to chatfragment

--- app/src/main/java/app/project/wishwash/chat/ChatFragment.java
+++ app/src/main/java/app/project/wishwash/chat/ChatFragment.java
@@ -11,8 +11,12 @@ import android.view.ViewGroup;
 
 
 import app.project.wishwash.R;
+import app.project.wishwash.chat.models.User;
 
 public class ChatFragment extends Fragment {
+    public interface ChatFragmentListener{
+        void onGuestUserChosen(User guest);
+    }
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,16 +34,16 @@ public class ChatFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param guestId Parameter 1.
+     * @param guestName Parameter 2.
      * @return A new instance of fragment ChatFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ChatFragment newInstance(String param1, String param2) {
+    public static ChatFragment newInstance(String guestId, String guestName) {
         ChatFragment fragment = new ChatFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM1, guestId);
+        args.putString(ARG_PARAM2, guestName);
         fragment.setArguments(args);
         return fragment;
     }
@@ -57,6 +61,6 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        return inflater.inflate(R.layout.activity_chat, container, false);
     }
 }
--- app/src/main/java/app/project/wishwash/chat/ChatShellActivity.java
+++ app/src/main/java/app/project/wishwash/chat/ChatShellActivity.java
@@ -1,74 +1,49 @@
-package app.project.wishwash.calender;
-
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.Calendar;
+package app.project.wishwash.chat;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import app.project.wishwash.R;
-import app.project.wishwash.booking.BookingContent;
 import app.project.wishwash.booking.BookingFragment;
-import app.project.wishwash.booking.NewBookingFragment;
-import app.project.wishwash.chat.ChatFragment;
+import app.project.wishwash.calender.CalendarFragment;
 import app.project.wishwash.chat.fragments.UserListFragment;
 import app.project.wishwash.chat.models.User;
 import app.project.wishwash.tips.TipsFragment;
 
-public class CalendarActivity extends AppCompatActivity implements CalendarFragment.CalendarFragmentListener, BookingFragment.OnListFragmentInteractionListener, UserListFragment.UserListFragmentListener {
-
-    private BottomNavigationView bottomNavigationView;
+public class ChatShellActivity extends AppCompatActivity implements UserListFragment.UserListFragmentListener, ChatFragment.ChatFragmentListener {
+    UserListFragment userListFragment;
     private Toolbar actionBar;
-    private final String TAG = "CalendarActivity";
+    private BottomNavigationView bottomNavigationView;
     private FragmentTransaction transaction;
-    private FragmentManager fragmentManager;
-    private SharedPreferences sp;
-    private CalendarFragment calendarFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate called");
-        setContentView(R.layout.activity_calendar);
+        setContentView(R.layout.activity_chat_shell);
 
-        calendarFragment = new CalendarFragment();
+        userListFragment = new UserListFragment();
 
         actionBar = findViewById(R.id.action_bar);
         bottomNavigationView = findViewById(R.id.BottomNavigation_Calendar);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
 
         setSupportActionBar(actionBar);
-
-        initCalendarFragment();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart called");
-
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.action_bar);
-    }
-
-    // Inflates --- CalendarFragment/fragment_calendar --- as the first fragment to be shown when app is started
-    private void initCalendarFragment() {
-        Fragment fragment = new CalendarFragment();
-        transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.FrameLayout_Calendar, fragment);
-        transaction.commit();
     }
 
     // When BottomNavigationView item is clicked, inflate belonging fragment.
@@ -93,27 +68,20 @@ public class CalendarActivity extends AppCompatActivity implements CalendarFragm
                     return false;
                 }
             };
-
     public void openFragment(Fragment fragment) {
         transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.FrameLayout_Calendar, fragment);
+        transaction.replace(R.id.FrameLayout_Chat, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
-
-    // To communicate from Fragment to Fragment through Activity
-    @Override
-    public void onDateChosen(Calendar c) {
-        openFragment(NewBookingFragment.newInstance(c.YEAR, c.MONTH, c.DAY_OF_MONTH));
-    }
-
-    @Override
-    public void onListFragmentInteraction(BookingContent.BookingItem item) {
-
-    }
-
+    //UserListFragment
     @Override
     public void onUserSent(User user) {
-
+//sop
+    }
+    //ChatFragment
+    @Override
+    public void onGuestUserChosen(User guest) {
+        openFragment(ChatFragment.newInstance(guest.getUserId(),guest.getUserName()));
     }
 }
