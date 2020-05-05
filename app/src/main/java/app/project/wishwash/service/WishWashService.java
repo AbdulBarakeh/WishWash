83fd6e6ce07ad2df7be86c1817bbe526fdcab6a7
AbdulBarakeh <abdulbarakeh98@gmail.com>
2020-05-05 14:45:15 +0200

    add empty service

---
+++ app/src/main/java/app/project/wishwash/service/WishWashService.java
@@ -0,0 +1,16 @@
+package app.project.wishwash.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class WishWashService extends Service {
+    public WishWashService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
